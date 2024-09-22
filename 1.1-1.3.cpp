9e8aa45762bf9c43c7d5381dc1f72fbdb44252e4
Ulises-Diaz <160899306+Ulises-Diaz@users.noreply.github.com>
2024-09-22 16:25:24 -0600

    Add files via upload

---
+++ 1.1-1.3.cpp
@@ -0,0 +1,46 @@
+
+1.1 - 1.2 talked about the history of c++ and its relation with C# and JAVA.
+
+1.3 Object-Oriented Programming is the reason we needed c++ instead of C. This chapter explain it.
+    OOP is organized around data (what is being affected)  "Data controlling acces to code"
+    you define data and routines that are permitted to act on that data. 
+        TRAITS:
+            Encapsulation: binds together code and data to manipulate. . "SELF-contained black box created"d
+            the code can be both private to the object or public. 
+                private: private data cant be accessed by a piece of the program that exists outside the object.
+                public: Public data can be accessed by other parts of the code. 
+                   this are used to give a controlled interface to the private elements of the object.
+            c++ encapsulation is a CLASS. Classes construct objects. 
+
+            Polymorphisim: the way of an interface to access a general class .
+                como si fuera una rueda. La rueda siempre es igual pero el mecanismo de girar la rueda es diferente
+            "one interface, multiple methods (functions)"
+            Polymorphisim helps reduce complexity by allowing the same interface to specify a general class
+
+            inheritance: object can acquire the properties of another object.  
+            Supongamos que tenemos una manzana roja. Esta manzana roja entra en la clase de manzanas.
+            Esta clase tiene ciertos atributos (comestible, nutritiva, limpia) pero la clase manzana
+            entra en otra categoria, la clase de frutas. Esta clase tiene otros atributos. Y esa misma
+            classe de fruta esta dentro de otra clase (comida) que tmb tiene otro atributos. Estos atributos 
+            se pasan a la clase fruta y esos a la clase manzana. Eso es inheritance. 
+
+***** BASICAMENTE FUE LA HISTORIA DE C++ Y SU RELACION CON OTROS LENGUAJES
+        TMB SE EXPLICO OOP **************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
